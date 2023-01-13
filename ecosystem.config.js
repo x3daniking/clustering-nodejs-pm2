2a82eb042fda0b41ac914203189feeef871eb01e
adnan khan <malikadnan655@gmail.com>
2023-01-13 16:35:40 +0500

    'pm2ecosystem'

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "clustering-nodejs-pm2",
+      script: "app.js",
+      instances: 0,
+      exec_mode: "cluster",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
